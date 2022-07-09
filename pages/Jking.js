d449f04115c1a7aa34a591e9ef55c38f5c6958f0
Jeremiah King <usaxray25@gmail.com>
2022-07-09 14:05:25 -0500

    first react with counter

---
+++ pages/Jking.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Loading from '../components/Loading';
+
+export default function Jking() {
+  return (
+    <div className="parent-text">
+      <Loading />
+      <div>Hello!</div>
+    </div>
+  );
+}
